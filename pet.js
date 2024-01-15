00591e121b15e51b17a549f36adfed8db2663b9d
Filippo2803 <filippoborelli8@gmail.com>
2024-01-15 15:08:05 +0100

    add other changes

--- pet.js
+++ pet.js
@@ -18,20 +18,15 @@ const pet1 = new Pet("Gigio", "Matteo", "cat", "Siamese")
 const pet2 = new Pet("Fufy", "Giovanni", "dog", "Labrador")
 console.log(pet1.idem())
 
-// devo fare in modo di creare col e card dinamicamente raccogliendo i dati inseriti nel form
-
-// 1) prendiamo dei riferimenti agli input
 const petName = document.getElementById('name')
 const ownerName = document.getElementById('ownerName')
 const species = document.getElementById('species')
 const breed = document.getElementById('breed')
 
-const saveButton = document.querySelector('form button') // il tag button dentro il tag form
+const saveButton = document.querySelector('form button')
 
-// 2) creo la mia rubrica in memoria
-const data = [] // un array di oggetti inizialmente vuoto
+const data = []
 
-// creo una classe Contact, per creare un oggetto a partire dai dati recuperati dal form
 class Data {
     constructor(_petname, _ownername, _species, _breed){
         this.petName = _petname
@@ -42,16 +37,15 @@ class Data {
 }
 
 const cards = function () {
-  // devo creare delle cards a partire dall'array di oggetti "contacts"
-  // 1) prendo un riferimento alla row
+
   const row = document.getElementById('data')
-  // svuoto la lista prima di ricreare le col
+
   row.innerHTML = ''
-  // 2) ciclo l'array contacts: per ogni contatto creo una col, creo una card e appendo il tutto
+
   data.forEach((data) => {
-    const newCol = document.createElement('div') // <div></div>
-    newCol.classList.add('col') // aggiungo la classe col
-    // riempio il contenuto della col
+    const newCol = document.createElement('div')
+    newCol.classList.add('col')
+
     newCol.innerHTML = `
         <div class="card">
             <div class="card-body">
@@ -62,22 +56,18 @@ const cards = function () {
             </div>
         </div>
         `
-    // appendere la col alla row
     row.appendChild(newCol)
   })
 
-  // svuoto il form ad operazione completata
   petName.value = ''
   ownerName.value = ''
   species.value = ''
   breed.value = ''
 }
 
-// 3) assegno un comportamento all'invio del form
 const formSubmit = document.getElementsByTagName('form')[0]
 formSubmit.addEventListener('submit', function (e) {
-  e.preventDefault() // fermiamo la pagina dal refresh
-  // raccolgo i dati dagli input, e ne creo un oggetto
+  e.preventDefault()
 
   const dati = new Data(
     petName.value,
@@ -87,8 +77,8 @@ formSubmit.addEventListener('submit', function (e) {
   )
 
   console.log('CARD CREATA', data)
-  // riempio la rubrica
+
   data.push(dati)
-  // popolare la seconda row della pagina con tante card quanti sono gli elementi di contacts
+
   cards()
 })
